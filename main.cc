98c817a4a503ffaf21c6e74c02d90e3d239c1f7e
root <root@localhost.localdomain>
2020-10-21 19:42:49 +0800

    demo

--- main.cc
+++ main.cc
@@ -2,7 +2,13 @@
 #include "Server.h"
 // #include "mem.h"
 
+void func(void* arg){
+    string* val = (string*)arg;
+    std::cout << *val;
+}
+
 int main(){
     httpServer::Server* s = new httpServer::Server(8080, 6);
+    s ->ResigterHandler("sad", func);
     s -> Start();
 }
